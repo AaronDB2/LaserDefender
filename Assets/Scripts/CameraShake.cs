ee27e0c290c0a5730b6cfc1c8db597f50d27e41e
Aaron de Boer <aarondeboerwk@gmail.com>
2022-10-08 23:12:09 +0200

    Added particle effect when hitting enemies or when getting hit by enemies. And added screen shake effect when the player gets hit

---
+++ Assets/Scripts/CameraShake.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3ce8032fd266b2a951d31a76ebde9f7b42d45d72e95eb1e6dcf32e888daa0cc2
+size 873
--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bef5b69277d2cd0b6ab4707530ef27aca4f0481a2dfc5b901d3158a8fc3a6aa9
-size 599
+oid sha256:81c4de7fb469ddd7bb35e8a446e2fe412370d8e1c50156e06f4689f8c1c0bab6
+size 1353
